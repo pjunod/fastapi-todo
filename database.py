641d92d7d019d0cbe87ff87e281e897335213a43
Paul Junod <pjunod@Pauls-MacBook-Pro.local>
2024-09-09 12:41:14 -0400

    Add unit tests etc

--- database.py
+++ database.py
@@ -1,24 +1,10 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-if os.getenv("DB") == "POSTGRES":
-    if os.getenv("CONTAINER") == "True":
-        SQLALCHEMY_DATABASE_URL = \
-            'postgresql://postgres:test123@host.docker.internal/TodoApplicationDatabase'
-    else:
-        SQLALCHEMY_DATABASE_URL = \
-            'postgresql://postgres:test123@localhost/TodoApplicationDatabase'
-else:
-    SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
-
-# Postgres
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-# SQLITE
-#engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
-import models
-from database import engine
-from routers import auth, todos, admin, users
+from fastapi import FastAPI, Request
+#from fastapi.templating import Jinja2Templates
+from .models import Base
+from .database import engine
+from .routers import auth, todos, admin, users
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+#templates = Jinja2Templates(directory="todoapp/templates")
+
+#@app.get("/")
+##def test(request: Request):
+#    return templates.TemplateResponse("home.html", {"request": request})
+
+@app.get("/healthy")
+def health_check():
+    return {'status': 'Healthy'}
+
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
