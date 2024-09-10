1794afcf2c9674323ccf96a49563269adfbd5e3a
Paul Junod <pjunod@Pauls-MacBook-Pro.local>
2024-09-10 12:52:44 -0400

    Refactor dir structure

---
+++ database.py
@@ -0,0 +1,21 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+if os.getenv("DB") == "POSTGRES":
+    if os.getenv("CONTAINER") == "True":
+        SQLALCHEMY_DATABASE_URL = \
+            'postgresql://postgres:test123@host.docker.internal/TodoApplicationDatabase'
+    else:
+        SQLALCHEMY_DATABASE_URL = \
+            'postgresql://postgres:test123@localhost/TodoApplicationDatabase'
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+else:
+    SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
